f9e53bbf4c7b1a1d08a5e346efd9131ca4090da4
Irina <vyustirina@gmail.com>
2024-07-31 22:38:26 +0300

    added App to-do list

---
+++ lesson/src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Hello,To-Do List</h1>
+    <input type="text" />
+    <button>OK</button>
+  <ul className="items">
+    <li className="item"> hvkjgbkj
+      <div>
+      <button>DONE</button>
+      <button>DELETE</button>
+      </div>
+    </li>
+  </ul>
+ 
+    </div>
+  );
+}
+
+export default App;
