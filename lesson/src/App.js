1dbefd7b38aefa94534b4fe706e16b6038408eea
Irina <vyustirina@gmail.com>
2024-08-02 00:04:09 +0300

    added useState for input and button

--- lesson/src/App.js
+++ lesson/src/App.js
@@ -1,12 +1,28 @@
-
+import React, {useState} from 'react';
 import './App.css';
 
+
 function App() {
+
+const [task, setTask] = useState('');
+
+
+const handleInputChange = (event) => {
+  setTask(event.target.value);
+}
+
+const handleAddTask = () => {
+  setTask('')
+}
+
   return (
     <div className="App">
     <h1>Hello,To-Do List</h1>
-    <input type="text" />
-    <button>OK</button>
+    <input type="text" 
+    value={task}
+    onChange={handleInputChange}
+    />
+    <button onClick={handleAddTask}>OK</button>
   <ul className="items">
     <li className="item"> hvkjgbkj
       <div>
